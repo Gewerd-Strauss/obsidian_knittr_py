f731264801e047a1b7ed09e6f915f02b41589311
Gewerd-Strauss <80548781+Gewerd-Strauss@users.noreply.github.com>
2024-11-11 22:13:00 +0100

    begin implementing `RenderingPipeline`-class

---
+++ obsidianknittrpy/modules/rendering/renderer.py
@@ -0,0 +1,21 @@
+class RenderingPipeline:
+    """
+    Rendering pipeline class responsible for taking the manuscript-string, and rendering it into the documents.
+    """
+
+    def __init__(self, file_string, debug=False):
+        """
+        Initialise the rendering-pipeline
+        :param file_string: String to qmd-file to be rendered.
+        """
+        self.debug = debug
+        self.file_string = file_string
+
+    def validate():
+        pass
+
+    def render():
+        """
+        Main rendering method.
+        """
+        pass
