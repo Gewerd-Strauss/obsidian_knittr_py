90089d71c14e4da1a6d81f83e2fb70297d8c66b1
Gewerd-Strauss <80548781+Gewerd-Strauss@users.noreply.github.com>
2024-11-18 19:07:17 +0100

    implement comandline-verb `version`

--- obsidianknittrpy/main.py
+++ obsidianknittrpy/main.py
@@ -12,6 +12,7 @@ from obsidianknittrpy.modules.commandline import (
 from obsidianknittrpy.modules.command_handlers import (
     handle_convert,
     handle_gui,
+    handle_version,
 )
 from obsidianknittrpy.modules.utility import (
     init_picknick_basket,
@@ -46,43 +47,46 @@ def main():
     )
     common_arguments(gui_parser)  # Reuse shared arguments for 'gui'
     gui_parser_setup(gui_parser)
-
+    # --- 'version' command setup ---
+    version_parser = subparsers.add_parser("version", help="Get the version.")
     args = parser.parse_args()
-    logging.basicConfig(level=args.loglevel)
-
-    pb = init_picknick_basket()
-    # Command handling
-    # 1. translate arguments
-    args = convert_format_args(args)
-
-    # 2. setup config-manager
-    RL.log("main", "inits", "default config")
-    CH = ConfigurationHandler(
-        last_run_path=None, loglevel=args["loglevel"], is_gui=True
-    )
-    CH.apply_defaults()
-    if args["custom_pipeline"] is not None:
-        CH.load_custom_pipeline(args["custom_pipeline"])
-    if args["custom_format_definitions"]:
-        CH.load_custom_format_definitions(args["custom_format_definitions"])
-    # 3. clear out work dir
-    if os.path.exists(CH.get_key("DIRECTORIES_PATHS", "work_dir")):
-        shutil.rmtree(CH.get_key("DIRECTORIES_PATHS", "work_dir"))
-        RL.log("main", "clears", CH.get_key("DIRECTORIES_PATHS", "work_dir"))
-    os.makedirs(CH.get_key("DIRECTORIES_PATHS", "work_dir"))
-    RL.add_log_location(CH.get_key("DIRECTORIES_PATHS", "work_dir"))
-    RL.log("main", "creates", CH.get_key("DIRECTORIES_PATHS", "work_dir"))
-    RL.log("main", "creates", RL.log_file)
-    if args["command"] == "convert":
-        # Parse pass-through arguments
-        print("implement commandline-pathway")
-        # Print all arguments in the desired format
-        # print_arguments(parsed_args)
-        handle_convert(args, pb, CH)
-    elif args["command"] == "gui":
-        handle_gui(args, pb, CH)
+    if args.command == "version":
+        handle_version()
     else:
-        parser.print_help()
+
+        # Command handling
+        # 1. translate arguments
+        args = convert_format_args(args)
+        logging.basicConfig(level=args["loglevel"])
+        pb = init_picknick_basket()
+        # 2. setup config-manager
+        RL.log("main", "inits", "default config")
+        CH = ConfigurationHandler(
+            last_run_path=None, loglevel=args["loglevel"], is_gui=True
+        )
+        CH.apply_defaults()
+        if args["custom_pipeline"] is not None:
+            CH.load_custom_pipeline(args["custom_pipeline"])
+        if args["custom_format_definitions"]:
+            CH.load_custom_format_definitions(args["custom_format_definitions"])
+        # 3. clear out work dir
+        if os.path.exists(CH.get_key("DIRECTORIES_PATHS", "work_dir")):
+            shutil.rmtree(CH.get_key("DIRECTORIES_PATHS", "work_dir"))
+            RL.log("main", "clears", CH.get_key("DIRECTORIES_PATHS", "work_dir"))
+        os.makedirs(CH.get_key("DIRECTORIES_PATHS", "work_dir"))
+        RL.add_log_location(CH.get_key("DIRECTORIES_PATHS", "work_dir"))
+        RL.log("main", "creates", CH.get_key("DIRECTORIES_PATHS", "work_dir"))
+        RL.log("main", "creates", RL.log_file)
+        if args["command"] == "convert":
+            # Parse pass-through arguments
+            print("implement commandline-pathway")
+            # Print all arguments in the desired format
+            # print_arguments(parsed_args)
+            handle_convert(args, pb, CH)
+        elif args["command"] == "gui":
+            handle_gui(args, pb, CH)
+        else:
+            parser.print_help()
 
 
 if __name__ == "__main__":
